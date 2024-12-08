2eafd510c588fcf1e1f09aff4145a9f051ef8bae
Jakub Jarosz <jakub@jarosz.dev>
2024-12-08 00:52:47 +0000

    Add 2nd hello example

---
+++ hello/2/hello.go
@@ -0,0 +1,11 @@
+package hello
+
+import "fmt"
+
+// ShareWith return a greeting string.
+func ShareWith(name string) string {
+	if name == "" {
+		name = "you"
+	}
+	return fmt.Sprintf("One for %s, one for me.", name)
+}
