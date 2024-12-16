3592069043aea19e04532ec3d56c7db91aaccb56
Jakub Jarosz <jakub@jarosz.dev>
2024-12-16 21:07:41 +0000

    Scrabble: Update 3rd approach

--- scrabble/3/scrabble.go
+++ scrabble/3/scrabble.go
@@ -7,25 +7,18 @@ import (
 // Score takes a string represending a scrabble word
 // and returns calculated score.
 func Score(word string) int {
+	letterScores := map[rune]int{
+		'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+		'D': 2, 'G': 2,
+		'B': 3, 'C': 3, 'M': 3, 'P': 3,
+		'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+		'K': 5,
+		'J': 8, 'X': 8,
+		'Q': 10, 'Z': 10,
+	}
 	score := 0
-	for _, r := range word {
-		switch unicode.ToUpper(r) {
-		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
-			score += 1
-		case 'D', 'G':
-			score += 2
-		case 'B', 'C', 'M', 'P':
-			score += 3
-		case 'F', 'H', 'V', 'W', 'Y':
-			score += 4
-		case 'K':
-			score += 5
-		case 'J', 'X':
-			score += 8
-		case 'Q', 'Z':
-			score += 10
-		default:
-		}
+	for _, letter := range word {
+		score += letterScores[rune(unicode.ToUpper(letter))]
 	}
 	return score
 }
