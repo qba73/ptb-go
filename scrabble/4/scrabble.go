97d5e61604508d2303861b1f93f9995e784211d0
Jakub Jarosz <jakub@jarosz.dev>
2024-12-16 21:08:09 +0000

    Scrable: add 4th approach

---
+++ scrabble/4/scrabble.go
@@ -0,0 +1,31 @@
+package scrabble
+
+import (
+	"unicode"
+)
+
+// Score takes a string represending a scrabble word
+// and returns calculated score.
+func Score(word string) int {
+	score := 0
+	for _, r := range word {
+		switch unicode.ToUpper(r) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score += 1
+		case 'D', 'G':
+			score += 2
+		case 'B', 'C', 'M', 'P':
+			score += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score += 4
+		case 'K':
+			score += 5
+		case 'J', 'X':
+			score += 8
+		case 'Q', 'Z':
+			score += 10
+		default:
+		}
+	}
+	return score
+}
